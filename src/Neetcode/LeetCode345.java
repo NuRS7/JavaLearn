de19b5bc31e30745dab74d0cb361123b829ec987
Nurs <nursultanerbakytuly88@gmail.com>
2025-03-04 01:43:11 +0600

    LeetCode java

---
+++ src/Neetcode/LeetCode345.java
@@ -0,0 +1,31 @@
+package Neetcode;
+
+public class LeetCode345 {
+    public static String reverseVowels(String s) {
+//        String r = "";
+//        s = s.toLowerCase();
+//        for (int i = 0; i < s.length(); i++) {
+//            char ch = s.charAt(i);
+////            if (s.charAt(i) = 'a' || s.charAt('e') || s.charAt('o') || s.charAt('i') || s.charAt('u')) {
+////                r = ch + s;,
+////            }
+//
+//
+//        }
+//        System.out.println(r);
+//        return r;
+
+
+        String r = "";
+        for (int i = 0; i < s.length(); i++) {
+            int left = s.charAt(0);
+            int right = s.charAt(s.length());
+            char ch = 'a','e', ''
+        }
+    }
+    public static void main(String[] args) {
+        String name = "Nursultan";
+        reverseVowels(name);
+
+    }
+}
