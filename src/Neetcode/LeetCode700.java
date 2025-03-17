aa4638a0213143d030ed16654d4cbf413d9b5bf0
Nurs <nursultanerbakytuly88@gmail.com>
2025-03-17 22:06:01 +0500

    LeetCode 700

---
+++ src/Neetcode/LeetCode700.java
@@ -0,0 +1,21 @@
+package Neetcode;
+
+import javax.swing.tree.TreeNode;
+
+public class LeetCode700 {
+    public static void main(String[] args) {
+
+    }
+    public static TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
