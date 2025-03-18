2dc7bff3ad9aaa3ce6842900a41d4306a9737526
Nurs <nursultanerbakytuly88@gmail.com>
2025-03-19 00:30:51 +0500

    LeetCode 700 fix errors

--- src/Neetcode/LeetCode700.java
+++ src/Neetcode/LeetCode700.java
@@ -1,21 +1,38 @@
 package Neetcode;
 
-import javax.swing.tree.TreeNode;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
 
 public class LeetCode700 {
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
 
-    }
-    public static TreeNode searchBST(TreeNode root, int val) {
-        if (root == null) {
-            return null;
-        }
-        if (root.val == val) {
-            return root;
-        } else if (root.val > val) {
-            return searchBST(root.left, val);
+        int val = 2;
+        TreeNode result = searchBST(root, val);
+        if (result != null) {
+            System.out.println("Found node with value: " + result.val);
         } else {
-            return searchBST(root.right, val);
+            System.out.println("Value not found in BST");
         }
+    }
+
+    public static TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+        return (val < root.val) ? searchBST(root.left, val) : searchBST(root.right, val);
     }
 }
