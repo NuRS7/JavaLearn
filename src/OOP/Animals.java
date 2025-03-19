0deff62891dfc04f8f6d69eec0fa7769d45ee1dc
Nurs <nursultanerbakytuly88@gmail.com>
2025-03-20 01:20:49 +0500

    inheritanceOOP

---
+++ src/OOP/Animals.java
@@ -0,0 +1,17 @@
+package OOP;
+
+public class Animals {
+    String name;
+
+    public Animals(String name) {
+        this.name = name;
+    }
+
+    public void sleep() {
+        System.out.println("I am sleeping");
+    }
+
+    public void eat() {
+        System.out.println("I am eating");
+    }
+}
