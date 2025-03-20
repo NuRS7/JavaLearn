e1a722f86af07efae325048242daa13226b44273
Nurs <nursultanerbakytuly88@gmail.com>
2025-03-21 04:03:05 +0500

    Interface repeat

--- src/OOP/ProgramWithInterface.java
+++ src/OOP/ProgramWithInterface.java
@@ -4,6 +4,13 @@ public class ProgramWithInterface {
     public static void main(String[] args) {
         Book book = new Book("Абай жолы", " Мухтар Ауезов");
         book.print();
+        Journal journal = new Journal("New magazine");
+        journal.print();
+
+        Printable p = new Journal("Hello");
+        p.print();
+        String name = ((Journal) p).getName();
+        System.out.println(name);
     }
 }
 interface Printable {
@@ -21,5 +28,20 @@ class Book implements Printable {
 
     public void print() {
         System.out.println(name+" "+author);
+    }
+}
+
+
+class Journal implements Printable {
+    private String name;
+
+    String getName() {
+        return name;
+    }
+    public Journal(String name) {
+        this.name = name;
+    }
+    public void print() {
+        System.out.println(name);
     }
 }
