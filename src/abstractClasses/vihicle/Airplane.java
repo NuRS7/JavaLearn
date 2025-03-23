9618134ec60ce5764cff6c594cd9f93133bf2c11
Nurs <nursultanerbakytuly88@gmail.com>
2025-03-23 14:14:37 +0500

    Java abstract classes

---
+++ src/abstractClasses/vihicle/Airplane.java
@@ -0,0 +1,8 @@
+package abstractClasses.vihicle;
+
+public class Airplane extends Vehicle {
+    public void move() {
+        System.out.println("Airplane move");
+    }
+
+}
---
+++ src/abstractClasses/vihicle/Car.java
@@ -0,0 +1,9 @@
+package abstractClasses.vihicle;
+
+public class Car extends Vehicle {
+
+    @Override
+    public void move() {
+        System.out.println("Car is moving");
+    }
+}
