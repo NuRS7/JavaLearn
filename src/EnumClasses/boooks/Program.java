1e729aa4a01f6fdacb50c2efc28f594afef580b7
Nurs <nursultanerbakytuly88@gmail.com>
2025-03-29 00:03:08 +0500

    ENUM classes

--- src/EnumClasses/boooks/Program.java
+++ src/EnumClasses/boooks/Program.java
@@ -4,6 +4,10 @@ public class Program {
     public static void main(String[] args) {
         Book book = new Book("Nur", "Author N", Genre.BELLETRE);
         System.out.println(book.getTitle() + " " + book.getAuthor() + " " + book.getType());
+        Genre[] type = Genre.values();
+        for (Genre genre : type) {
+            System.out.println(genre);
+        }
 
     }
 }
