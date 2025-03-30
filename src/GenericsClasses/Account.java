46cb1e9821921dba5c0f44e4d6338987e5dc8e1d
Nurs <nursultanerbakytuly88@gmail.com>
2025-03-30 23:59:18 +0500

    Interfaces Generics

---
+++ src/GenericsClasses/Account.java
@@ -0,0 +1,26 @@
+package GenericsClasses;
+
+public class Account implements Accountable<String> {
+    private String id;
+    private int sum;
+
+    public Account(String id, int sum) {
+        this.id = id;
+        this.sum = sum;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public int getSum() {
+        return sum;
+    }
+
+    @Override
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+}
