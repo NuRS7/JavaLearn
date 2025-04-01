52151a6bbeb9bd620027a0cfd678af0134c90b12
Nurs <nursultanerbakytuly88@gmail.com>
2025-04-01 23:56:10 +0500

    LeetCode28

---
+++ src/Neetcode/LeetCode28.java
@@ -0,0 +1,15 @@
+package Neetcode;
+
+public class LeetCode28 {
+    public static int strStr(String haystack, String needle) {
+        System.out.println(haystack.toCharArray());
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String haystack = "sadbutsad";
+        String needle = "sad";
+        strStr(haystack, needle);
+    }
+}
